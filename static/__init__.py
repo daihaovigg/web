108dcbc1f345aa88f9b311d8192d32f915d14237
daihaovigg <865058222@qq.com>
2015-07-02 19:44:19 +0800

    origin code

--- static/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#-*-coding:utf-8-*-
-'''
-    Something running need
-'''
-__author__ = 'dai'
--- static/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*-coding:utf-8-*-
-
-from captcha import mycaptcha
-s=mycaptcha()
-print s.code
-s.reload()
-s.save()
