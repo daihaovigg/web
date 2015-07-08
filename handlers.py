fc5bf6e0c251ed624febf1264e4f7705bdf0574a
daihaovigg <865058222@qq.com>
2015-07-08 19:19:45 +0800

    some magic so it can work well but i know nothing

--- handlers.py
+++ handlers.py
@@ -223,7 +223,8 @@ def api_delete_comments(id, request):
     return dict(id=id)
 
 @get('/api/users')
-def api_get_users(*, page='1'):
+def api_get_users(request,*, page='1'):
+    check_admin(request)
     page_index = get_page_index(page)
     num = yield from User.findNumber('count(id)')
     p = Page(num, page_index)
